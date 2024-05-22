522bd480341b66d610211a9ac68362b650d6706a
gfteix <gabriel.fteixeira13@gmail.com>
2024-05-21 22:03:27 -0300

    feat: improve css for login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,18 +1,20 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
 import './LoginPage.css'
 
-const ProductPage = () => {
+const LoginPage = () => {
   return (
    <main className="login-page-container">
-        <div className="welcome-container">
-            <h2>Login</h2>
-            <br></br><br></br>
-            <h3>Olá, bem vindo ao Mercado Verde!</h3>
+        <div className="login-content-container">
+          <div className="welcome-container">
+              <h2>Login</h2>
+              <br></br><br></br>
+              <h3 className="welcome-message">Olá, seja bem vindo ao <a className="mercado-verde">Mercado Verde!</a></h3>
+          </div>
+          <LoginForm/>
+          <p>Não tem uma conta? <a href="">Cadastre-se</a></p>
         </div>
-        <LoginForm/>
-        <p>Não tem uma conta? <a>Cadastre-se</a></p>
    </main>
   );
 };
 
-export default ProductPage;
+export default LoginPage;
